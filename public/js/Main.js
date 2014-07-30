8277df22e4b64e7c93efe63c0cc9c3b23a58f1ee
Amin Meyghani <amin.meyghani@quovo.com>
2014-07-29 21:14:06 -0400

    add basics of bootstrap ui

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,19 +1,27 @@
-function Main($scope){
-  $scope.users = [
-    {id: 1, name: "Tom", age: "22"},
-    {id: 2, name: "Jane", age: "23"},
-    {id: 3, name: "Kim", age: "24"}
+angular.module('MainApp', ['ui.bootstrap'])
+.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.data = "some data2";
+
+}])
+
+.controller('DropdownCtrl', ['$scope', function($scope) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
   ];
-  $scope.removeUser = function (u) {
-    $scope.users = $scope.users.filter(function(user){
-      return user.id !== u.id;
-    });
+
+  $scope.status = {
+    isopen: false
   };
-  // $scope.removeUser = function (u,i){
-  //   $scope.users.forEach(function(user){
-  //     if (u.id === user.id){
-  //       $scope.users.splice(i--, 1)
-  //     }
-  //   });
-  // };
-}
+
+  $scope.toggled = function(open) {
+    console.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+}])
